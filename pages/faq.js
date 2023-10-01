aa722719849dfd40a9829a2634247f817e484d35
Polyane Tuag <pollyportugues@hotmail.com>
2023-10-01 12:46:17 -0300

    feat: page faq

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Alura Cases - Página de perguntas FAQ</h1>
+            <a href="/">Ir para a Home</a>
+        </div>
+    )
+}
