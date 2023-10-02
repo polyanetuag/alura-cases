d1a2b7d2dd7e152806748ab68f867fc5023baaa2
Polyane Tuag <pollyportugues@hotmail.com>
2023-10-02 10:39:44 -0300

    fix: using link component

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Alura Cases - Página de perguntas FAQ</h1>
-            <a href="/">Ir para a Home</a>
-        </div>
-    )
+import Link from "../src/components/Link";
+
+export default function FaqPage() {
+  return (
+    <div>
+      <h1>Alura Cases - Página de perguntas FAQ</h1>
+      <Link href="/">
+       Ir para a Home
+      </Link>
+    </div>
+  );
 }
