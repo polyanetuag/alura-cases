22058f4f9ba27f4f6126cd2e7b37e9f38363858a
Polyane Tuag <pollyportugues@hotmail.com>
2023-10-03 11:10:40 -0300

    feat: added styles title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Link from "../src/components/Link";
 
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+       ${Tag} {
+          color: red;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </>
+  );
+}
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
-     <Link href='/faq'>Ir para a página do FAQ</Link>
+      <Title as="h2">Alura Cases - Home</Title>
+      <Link href="/faq">Ir para a página do FAQ</Link>
     </div>
   );
 }
