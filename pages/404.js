d64dfb15977e966a55c18b2e1a34522aa0a1994c
Polyane Tuag <pollyportugues@hotmail.com>
2023-10-25 19:02:13 -0300

    fix: message

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Link from "../src/components/Link";
 export default function Page404() {
     return (
         <div>
-            <h1>Você se perdeu e caiu na página 404 😔</h1>
+            <h1>Ops! Você se perdeu e caiu na página 404 😔</h1>
             <Link href="/">
                 <a>Ir para a home</a>
             </Link>
