cd3b0cee0a54756739081652918ebf16da4f2ba5
Polyane Tuag <pollyportugues@hotmail.com>
2023-10-25 19:33:28 -0300

    fix: changed page faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,29 @@
 import Link from "../src/components/Link";
+import { useState, useEffect } from "react";
 
 export default function FaqPage() {
+  const [faq, setFaq] = useState([]);
+
+  useEffect(() => {
+    const FAQ_API_URL =
+      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+    fetch(FAQ_API_URL)
+      .then((respostaDoServidor) => {
+        return respostaDoServidor.json();
+      })
+      .then((resposta) => {
+        setFaq(resposta);
+      });
+  }, []);
   return (
     <div>
       <h1>Alura Cases - Página de perguntas FAQ</h1>
-      <Link href="/">
-       Ir para a Home
-      </Link>
+      <Link href="/">Ir para a Home</Link>
+      <ul>
+        {faq.map(({ question }) => (
+          <li>{question}</li>
+        ))}
+      </ul>
     </div>
   );
 }
