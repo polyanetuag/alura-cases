da56788336be00c229a197d331b9818d9a59e228
Polyane Tuag <pollyportugues@hotmail.com>
2023-10-25 19:47:08 -0300

    fix: using getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,20 +1,28 @@
 import Link from "../src/components/Link";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-export default function FaqPage() {
-  const [faq, setFaq] = useState([]);
+export async function getStaticProps() {
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
 
-  useEffect(() => {
-    const FAQ_API_URL =
-      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
-    fetch(FAQ_API_URL)
-      .then((respostaDoServidor) => {
-        return respostaDoServidor.json();
-      })
-      .then((resposta) => {
-        setFaq(resposta);
-      });
-  }, []);
+  return {
+    props: {
+      faq
+    },
+  };
+}
+
+export default function FaqPage({faq}) {
+  // const [faq, setFaq] = useState([]);
+
+  // useEffect(() => {}, []);
   return (
     <div>
       <h1>Alura Cases - Página de perguntas FAQ</h1>
@@ -25,7 +33,6 @@ export default function FaqPage() {
             <h2>{question}</h2>
             <p>{answer}</p>
           </article>
-          
         ))}
       </ul>
     </div>
