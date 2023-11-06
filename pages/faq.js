356d2b9bf2fb4fe5f2993bf8f5a576146876a6c2
Polyane Tuag <pollyportugues@gmail.com>
2023-11-06 18:40:35 -0300

    fix: page FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,23 @@
-import Link from "../src/components/Link";
+import FaqScreen from '../src/screens/FaqScreen';
 
-export default function FaqPage() {
-  return (
-    <div>
-      <h1>Alura Cases - Página de perguntas FAQ</h1>
-      <Link href="/">
-       Ir para a Home
-      </Link>
-    </div>
-  );
-}
+export default FaqScreen; 
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
